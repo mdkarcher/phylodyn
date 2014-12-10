3ed5c13099783395849d57579df087b0dc39ade2
Michael Karcher <michael.d.karcher@gmail.com>
2014-12-09 16:50:20 -0800

    Addes relwid_emp, which calculates (average) relative width of the 95% credible interval, relative to the median rather than the truth.

--- R/INLA_tools.R
+++ R/INLA_tools.R
@@ -41,6 +41,19 @@ relwid = function(INLA_out, traj, hilim=Inf, lolim=0, yhilim=Inf, ylolim=0)
   return(list(tot = result, avg = result / n ))
 }
 
+relwid_emp = function(INLA_out, hilim=Inf, lolim=0, yhilim=Inf, ylolim=0)
+{
+  mod = INLA_out$result$summary.random$time
+  mask = mod$ID <= hilim & mod$ID >= lolim & traj(mod$ID) <= yhilim & traj(mod$ID) >= lolim
+  grid_pts = mod$ID[mask]
+  n = length(grid_pts)
+  med = exp(-mod$"0.5quant"[mask])
+  lo = exp(-mod$"0.975quant"[mask])
+  hi = exp(-mod$"0.025quant"[mask])
+  result = sum( (hi - lo)/med )
+  return(list(tot = result, avg = result / n ))
+}
+
 envelope = function(INLA_out, traj, hilim=Inf, lolim=0, yhilim=Inf, ylolim=0)
 {
   mod = INLA_out$result$summary.random$time
