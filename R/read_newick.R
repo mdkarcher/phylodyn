e74ee29afa417da5b83c4dc46c32a2c8a18e2572
Michael Karcher <michael.d.karcher@gmail.com>
2015-09-04 15:51:43 -0700

    Julia added tolerance argument to heterochronous_gp_stat.

--- R/read_newick.R
+++ R/read_newick.R
@@ -46,7 +46,42 @@ branching_sampling_times <- function(phy)
   return(branching_sampling_times)
 }
 
-heterochronous_gp_stat <- function(phy)
+heterochronous_gp_stat <- function(phy, tol=.1)
+{
+  #Update Aug 2015 by Julia. Adhoc for simulation with a tolerance parameters
+  b.s.times = branching_sampling_times(phy)
+  int.ind = which(as.numeric(names(b.s.times)) < 0)
+  tip.ind = which(as.numeric(names(b.s.times)) > 0)
+  num.tips = length(tip.ind)
+  num.coal.events = length(int.ind)
+  sampl.suf.stat = rep(NA, num.coal.events)
+  coal.interval = rep(NA, num.coal.events)
+  coal.lineages = rep(NA, num.coal.events)
+  sorted.coal.times = sort(b.s.times[int.ind])
+  names(sorted.coal.times) = NULL
+  sampling.times = sort((b.s.times[tip.ind]))
+  
+  for (i in 2:length(sampling.times))
+  {
+    if ((sampling.times[i] - sampling.times[i - 1]) < tol)
+    {
+      sampling.times[i] <- sampling.times[i - 1]
+    }
+  }
+  
+  unique.sampling.times <- unique(sampling.times)
+  sampled.lineages = NULL
+  
+  for (sample.time in unique.sampling.times)
+  {
+    sampled.lineages = c(sampled.lineages, sum(sampling.times == sample.time))
+  }
+  
+  return(list(coal_times = sorted.coal.times, s_times = unique.sampling.times,
+              n_sampled = sampled.lineages))
+}
+
+heterochronous_gp_stat_old <- function(phy)
 {
   b.s.times = branching_sampling_times(phy)
   int.ind = which(as.numeric(names(b.s.times)) < 0)
