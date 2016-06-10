40ec7680b2fdf62efd772be659fd80c3c4ae79aa
Michael Karcher <michael.d.karcher@gmail.com>
2016-06-10 09:47:46 -0700

    Commented a function that was identical to another function in phylodyn, preventing compiling.

--- R/coal_lik_BSMC.R
+++ R/coal_lik_BSMC.R
@@ -101,55 +101,54 @@ get_F_values<-function(latent,init,Fl){
   return(myF.list)
 }
     
-coal_lik_init = function(samp_times, n_sampled, coal_times, grid)
-{
-  ns = length(samp_times)
-  nc = length(coal_times)
-  ng = length(grid)-1
-  
-  if (length(samp_times) != length(n_sampled))
-    stop("samp_times vector of differing length than n_sampled vector.")
-  
-  if (length(coal_times) != sum(n_sampled) - 1)
-    stop("Incorrect length of coal_times: should be sum(n_sampled) - 1.")
-  
-  if (max(samp_times, coal_times) > max(grid))
-    stop("Grid does not envelop all sampling and/or coalescent times.")
-  
-  t = sort(unique(c(samp_times, coal_times, grid)))
-  l = rep(0, length(t))
-  
-  for (i in 1:ns)
-    l[t >= samp_times[i]] = l[t >= samp_times[i]] + n_sampled[i]
-  
-  for (i in 1:nc)
-    l[t >= coal_times[i]] = l[t >= coal_times[i]] - 1
-  
-  #print(l)
-  
-  if (sum((l < 1) & (t >= min(samp_times))) > 0)
-    stop("Number of active lineages falls below 1 after the first sampling point.")
-  
-  mask = l > 0
-  t = t[mask]
-  l = head(l[mask], -1)
-  
-  gridrep = rep(0, ng)
-  for (i in 1:ng)
-    gridrep[i] = sum(t > grid[i] & t <= grid[i+1])
-  
-  
-  C = 0.5 * l * (l-1)
-  D = diff(t)
-  
-  y = rep(0, length(D))
-  y[t[-1] %in% coal_times] = 1
-  
-  rep_idx = cumsum(gridrep)
-  rep_idx = cbind(rep_idx-gridrep+1,rep_idx)
-  
-  return(list(t=t, l=l, C=C, D=D, y=y, gridrep=gridrep, ng=ng, rep_idx=rep_idx, args=list(samp_times=samp_times, n_sampled=n_sampled, coal_times=coal_times, grid=grid)))
-}
+# coal_lik_init = function(samp_times, n_sampled, coal_times, grid)
+# {
+#   ns = length(samp_times)
+#   nc = length(coal_times)
+#   ng = length(grid)-1
+#   
+#   if (length(samp_times) != length(n_sampled))
+#     stop("samp_times vector of differing length than n_sampled vector.")
+#   
+#   if (length(coal_times) != sum(n_sampled) - 1)
+#     stop("Incorrect length of coal_times: should be sum(n_sampled) - 1.")
+#   
+#   if (max(samp_times, coal_times) > max(grid))
+#     stop("Grid does not envelop all sampling and/or coalescent times.")
+#   
+#   t = sort(unique(c(samp_times, coal_times, grid)))
+#   l = rep(0, length(t))
+#   
+#   for (i in 1:ns)
+#     l[t >= samp_times[i]] = l[t >= samp_times[i]] + n_sampled[i]
+#   
+#   for (i in 1:nc)
+#     l[t >= coal_times[i]] = l[t >= coal_times[i]] - 1
+#   
+#   #print(l)
+#   
+#   if (sum((l < 1) & (t >= min(samp_times))) > 0)
+#     stop("Number of active lineages falls below 1 after the first sampling point.")
+#   
+#   mask = l > 0
+#   t = t[mask]
+#   l = head(l[mask], -1)
+#   
+#   gridrep = rep(0, ng)
+#   for (i in 1:ng)
+#     gridrep[i] = sum(t > grid[i] & t <= grid[i+1])
+#   
+#   C = 0.5 * l * (l-1)
+#   D = diff(t)
+#   
+#   y = rep(0, length(D))
+#   y[t[-1] %in% coal_times] = 1
+#   
+#   rep_idx = cumsum(gridrep)
+#   rep_idx = cbind(rep_idx-gridrep+1,rep_idx)
+#   
+#   return(list(t=t, l=l, C=C, D=D, y=y, gridrep=gridrep, ng=ng, rep_idx=rep_idx, args=list(samp_times=samp_times, n_sampled=n_sampled, coal_times=coal_times, grid=grid)))
+# }
 
 
 
