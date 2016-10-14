f842c856692b1a7d2fdea5e23cda12e4da8b7d39
Michael Karcher <michael.d.karcher@gmail.com>
2016-10-13 21:48:06 -0700

    Changed order of arguments for pref_sample and documented.

--- R/statgentools.R
+++ R/statgentools.R
@@ -1,9 +1,27 @@
+#' Sample preferentially
+#' 
+#' Sample values from an inhomogeneous Poisson process with intensity
+#'   c*f(t)^beta.
+#' 
+#' @param f function to be sampled from preferentially.
+#'
+#' @param lim lower and upper sampling bounds. Defaults to c(0,1).
+#' @param c numeric constant of proportionality for intensity. Defaults to 1.
+#' @param beta numeric power to which f is raised for intensity. Defaults to 1.
+#' @param upper numeric upper bound for function between limits defined by lim.
+#'   Used in thinning algorithm.
+#' @param grid.len integer if upper is undefined, automatically evaluates f at
+#'   grid.len points between min(lim) and max(lim) to find upper bound.
+#'
 #' @export
-pref_sample = function(f, c=1, lim=c(0,1), grid.len = 1000, beta=1)
+pref_sample = function(f, lim=c(0,1), c=1, beta=1, upper=NULL, grid.len = 1000)
 {
-  grid = seq(lim[1], lim[2], length.out=grid.len)
-  eval = f(grid)^beta
-  upper = max(eval)
+  if (is.null(upper))
+  {
+    grid = seq(lim[1], lim[2], length.out=grid.len)
+    eval = f(grid)^beta
+    upper = max(eval)
+  }
   
   nfull = rpois(1, c * upper * diff(lim))
   pfull = sort(runif(nfull, lim[1], lim[2]))
