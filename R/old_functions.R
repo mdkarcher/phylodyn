2874ee5c33d2279bc3a69efc1e5cfb8de7f3b4ae
Michael Karcher <michael.d.karcher@gmail.com>
2016-11-02 13:24:33 -0700

    Deprecated coalgen_thinning_hetero (coalsim replaces it) to prepare for CRAN submission.

--- R/old_functions.R
+++ R/old_functions.R
@@ -948,3 +948,176 @@
 #   
 #   return(list(t=t, l=l, C=C, D=D, y=y, gridrep=gridrep, ns=sum(n_sampled), nc=nc, ng=ng, rep_idx=rep_idx, args=list(samp_times=samp_times, n_sampled=n_sampled, coal_times=coal_times, grid=grid)))
 # }
+#
+
+# #' Generate inhomogeneous coalescent with heterochronous samples.
+# #' 
+# #' @param sample a matrix with 2 columns. The first column contains the number 
+# #'   of samples collected at the time defined in the second column.
+# #' @param traj_inv a function returning the one over the effective population 
+# #'   size.
+# #' @param upper numeric. The maximum of the traj_inv function on the relevant 
+# #'   interval.
+# #' @param ... additional parameters to pass to the traj_inv function.
+# #'   
+# #' @return A list containing coalescent times \code{coal_times}, intercoalescent
+# #'   times \code{intercoal_times}, and number of active lineages between
+# #'   coalescent times \code{lineages}.
+# #' @export
+# #' 
+# #' @examples
+# #' coalgen_thinning_hetero(cbind(c(10,3), c(0,5)), unif_traj_inv, 0.01)
+# coalgen_thinning_hetero <- function(sample,traj_inv,upper,...)
+# {
+#   # sample = is a matrix with 2 columns. The first column contains the number of samples collected at the time defined in the second column
+#   # traj_inv = the effective population size function
+#   # this works for heterochronous sampling
+#   # assumes sample[1,1]>1
+#   samp_times = sample[,2]
+#   n_sampled = sample[,1]
+#   
+#   s=sample[1,2]
+#   b <- sample[1,1]
+#   n <- sum(sample[,1])-1
+#   m <- n
+#   nsample <- nrow(sample)
+#   sample <- rbind(sample,c(0,10*max(sample,2))) # Bug?
+#   out <- rep(0,n)
+#   branches <- rep(0,n)
+#   i <- 1
+#   while (i<(nsample))
+#   {
+#     #if (b==1)
+#     #{
+#     #  break
+#     #}
+#     if (b<2)
+#     {
+#       b <- b+sample[i+1,1]
+#       s <- sample[i+1,2]
+#       i <- i+1
+#     }
+#     E <- stats::rexp(1,upper*b*(b-1)*.5)
+#     if (stats::runif(1) <= traj_inv(E+s,...)/upper)
+#     {
+#       if ( (s+E)>sample[i+1,2])
+#       {
+#         b <- b+sample[i+1,1]
+#         s <- sample[i+1,2]
+#         i <- i+1
+#       }
+#       else
+#       {
+#         s <- s+E
+#         out[m-n+1] <- s
+#         branches[m-n+1] <- b
+#         n <- n-1
+#         b <- b-1
+#       }
+#     }
+#     else
+#     {
+#       s <- s+E
+#     }    
+#   }
+#   
+#   while (b>1)
+#   { 
+#     E <- stats::rexp(1,upper*b*(b-1)*.5)
+#     if (stats::runif(1)<=traj_inv(E+s,...)/upper)
+#     {
+#       s <- s+E
+#       out[m-n+1] <- s
+#       branches[m-n+1] <- b
+#       n <- n-1
+#       b <- b-1
+#     }
+#     else
+#     {
+#       s <- s+E
+#     }
+#   }
+#   
+#   return(list(intercoal_times=c(out[1],diff(out)), lineages=branches, 
+#               coal_times=out, samp_times = samp_times, n_sampled = n_sampled))   
+# }
+# 
+# coalgen_thinning_iso <- function(sample,traj_inv,upper=25,...)
+# {
+#   ###Need to add correction to "systematic" definition of upper bound
+#   s=sample[2]
+#   n <- sample[1]
+#   out <- rep(0,n-1)
+#   time <- 0
+#   j <- n
+#   while (j>1)
+#   {
+#     time <- time+stats::rexp(1,upper*j*(j-1)*.5)
+#     if (stats::runif(1)<=1/(traj_inv(time,...)*upper))
+#     {
+#       out[n-j+1] <- time
+#       j <- j-1
+#     }
+#   }
+#   return(list(intercoal_times=c(out[1],diff(out)),lineages=seq(n,2,-1), coal_times=out))
+# }
+# 
+# coalgen_transformation_hetero <- function(sample, trajectory,val_upper=10)
+# {
+#   # sample = is a matrix with 2 columns. The first column contains the number of samples collected at the time defined in the second column
+#   # trajectory = one over the effective population size function
+#   # this works for heterochronous sampling
+#   # assumes sample[1,1]>1
+#   s=sample[1,2]
+#   b <- sample[1,1]
+#   n <- sum(sample[,1])-1
+#   m <- n
+#   nsample <- nrow(sample)
+#   sample <- rbind(sample,c(0,10))
+#   out <- rep(0,n)
+#   branches <- rep(0,n)
+#   i <- 1
+#   while (i<(nsample+1))
+#   {
+#     #if (b==1)
+#     #{
+#     #  break
+#     #}
+#     if (b<2)
+#     {
+#       b <- b+sample[i+1,1]
+#       s <- sample[i+1,2]
+#       i <- i+1
+#     }
+#     x <- stats::rexp(1)
+#     f <- function(bran,u,x,s) .5*bran*(bran-1)*stats::integrate(trajectory, s, s+u)$value - x    
+#     y <- stats::uniroot(f,bran=b,x=x,s=s,lower=0,upper=val_upper)$root
+#     while ( (s+y)>sample[i+1,2])
+#     {
+#       #     f <- function(bran,u,x,s) .5*bran*(bran-1)*stats::integrate(trajectory, s, s+u)$value - x    
+#       #     y <- stats::uniroot(f,bran=b,x=x,s=s,lower=0,upper=val_upper)$root
+#       x <- x-.5*b*(b-1)*stats::integrate(trajectory,s,sample[i+1,2])$value  
+#       b <- b+sample[i+1,1]
+#       s <- sample[i+1,2]
+#       i <- i+1
+#       f <- function(bran,u,x,s) .5*bran*(bran-1)*stats::integrate(trajectory, s, s+u)$value - x    
+#       y <- stats::uniroot(f,bran=b,x=x,s=s,lower=0,upper=val_upper)$root
+#       if (i==nsample)
+#       {
+#         sample[nsample+1,2] <- 10*(s+y)
+#       }
+#     } 
+#     
+#     s <- s+y
+#     out[m-n+1] <- s
+#     branches[m-n+1] <- b
+#     n <- n-1
+#     b <- b-1
+#     if (i==nsample)
+#     {
+#       sample[nsample+1,2] <- 10*(s+y)
+#     }
+#   }
+#   
+#   return(list(branches=c(out[1],diff(out)),lineages=branches))   
+# }
